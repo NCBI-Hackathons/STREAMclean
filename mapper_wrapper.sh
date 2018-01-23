81acb91449c09b27ae5254c74c1c6975956f77c8
kbshimmyo <bethshimmyo@gmail.com>
2018-01-23 16:45:50 +0000

    Added rudimentary flow control and validation (not touching bioinformatics functionality).  This is subject to change based on command-line options getting added/removed.

--- mapper_wrapper.sh
+++ mapper_wrapper.sh
@@ -2,6 +2,25 @@
 
 MAGIC_BLAST_DIR=/home/ubuntu/bastian/ncbi-magicblast-1.3.0
 WORK_DIR="$(pwd)"
+programname=$0
+
+function usage {
+	echo "usage:  $programname [-d existingDBName] [-e excludeList] [-i includeList] [-m  magicBlastDir] [-o outDir] [-w workDir]"
+	echo "  -d existingDBName	Specify an existing database for makeblast to look at, to avoid downloading genomes."
+	echo "  -e excludeList	Blacklist of taxonomy indicators - magicBlast will produce only [pieces] that are not included in these genomes."
+	echo "  -i includeList	Whitelist of taxonomy indicators - magicBlast will produce only [pieces] that are included in these genomes."
+	echo "			Blacklist and whitelist should be of the following format: [Add format options]."
+	echo ""
+	echo "  -m magicBlastDir	Specify the directory that contains the bin/magicblast - default is "$WORK_DIR
+	echo "  -o outDir		Specify the directory for output. [What goes here?]"
+	echo "  -w workDir		Specify the working directory to create the genome database in - default is the current directory."
+}
+
+if [ $# -eq 0 ]; then
+	usage
+	exit 0
+fi
+
 
 while getopts ":d:e:i:m:o:w:" o; do
     case "${o}" in
@@ -23,12 +42,25 @@ while getopts ":d:e:i:m:o:w:" o; do
 	w)
 	    WORK_DIR=${OPTARG}
 	    ;;
-        *)
+	:)
+	   echo  "Invalid option: $OPTARG requires an argument"
+	   usage
+	   exit 0
+	   ;;
+        \?)
             usage
+	    exit 0
             ;;
     esac
 done
 shift $((OPTIND-1))
+
+# nice validation to do:  Require exactly one of d, e, i  flags
+
+if [ ! -d "$WORK_DIR" ]; then
+  echo "$WORK_DIR" does not exist - exiting.
+  exit 0
+fi
 
 echo combined fasta and db files will be in this working directory: "$WORK_DIR"
 echo "$BLAST_DB_NAME"
